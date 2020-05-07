f5281a7451140c5b5e294e3868a31ca7f4c82493
pauleanderson <pauleanderson@gmail.com>
2020-05-07 18:19:24 +0000

    update

--- ngm/base.py
+++ ngm/base.py
@@ -3,20 +3,23 @@
 
 # Initialize Pytorch
 
-import torch
+from torch.utils.data import Dataset
 import torch.nn as nn
-import torchvision.datasets as dsets
-import torchvision.transforms as transforms
-from torch.autograd import Variable
+import torch
 
-import pandas as pd
-import numpy as np
+class MyDataset(Dataset):
+    def __init__(self, values, labels):
+        super(MyDataset, self).__init__()
+        self.values = values
+        self.labels = labels
+    def __len__(self):
+        return len(self.values)  # number of samples in the dataset
+    def __getitem__(self, index):
+        return self.values[index], self.labels[index]
 
-import copy
-
-class Net1(nn.Module):
+class Net(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
-        super(Net1 , self).__init__()                    # Inherited from the parent class nn.Module
+        super(Net, self).__init__()                    # Inherited from the parent class nn.Module
         self.fc1 = nn.Linear(input_size, hidden_size)  # 1st Full-Connected Layer: 10312 (input data) -> 50 (hidden node)
         self.relu = nn.ReLU()                          # Non-Linear ReLU Layer: max(0,x)
         self.fc2 = nn.Linear(hidden_size, num_classes) # 2nd Full-Connected Layer: 50 (hidden node) -> 39 (output class)
@@ -27,34 +30,34 @@ class Net1(nn.Module):
         out = self.fc2(out)
         return out
     
-def train(model,dataloader,**kwargs):
-    # model is such as Net1
-    
-    default_kwargs = {
-        'num_epochs': 5,        # The number of times entire dataset is trained
-        'batch_size': 10,       # The size of input data took for one iteration
-        'learning_rate':0.001,  # The speed of convergence
-    }
-    
-    options = {**default_kwargs,**kwargs}
 
+def train(model,A,dataloader,dataset,learning_rate,num_epochs):
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=options['learning_rate'])
-
-    for epoch in range(options['num_epochs']):
-        for i, (images, labels) in enumerate(dataloader):     # Load a batch of images with its (index, data, class)
-            images = Variable(images.view(-1, 1*10312))       # Convert torch tensor to Variable: change image from a vector of size 784 to a matrix of 28 x 28
-            labels = Variable(labels)
-
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+    
+    for epoch in range(num_epochs):
+        for i, (X, labels) in enumerate(dataloader):   # Load a batch of images with its (index, data, class)
+            batch_size = len(labels)
             optimizer.zero_grad()                             # Intialize the hidden weight to all zeros
-            outputs = model(images.float())                   # Forward pass: compute the output class given a image
-            #if epoch == num_epochs-1:
-            #    print(outputs, labels.long())
-            loss = criterion(outputs, labels.long())          # Compute the loss: difference between the output class and the pre-given label
+            outputs = model(X)                   # Forward pass: compute the output class given a image
+            try:
+                loss = criterion(outputs, labels.long())          # Compute the loss: difference between the output class and the pre-given label
+            except:
+                import pdb; pdb.set_trace()
 
             loss.backward()                                   # Backward pass: compute the weight
             optimizer.step()                                  # Optimizer: update the weights of hidden nodes
 
-            if (i+1) % 100 == 0:                              # Logging
+            if (i+1) % 20 == 0:                              # Logging
                 print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' %(epoch+1, num_epochs, i+1, len(dataset)//batch_size, loss.item()))
+
+def prepare_for_evaluation(model,testloader):
+    all_predicted = []
+    all_labels = []
+    for i, (X, labels) in enumerate(testloader):
+        outputs = model(X) 
+        _, predicted = torch.max(outputs.data, 1)  # Choose the best class from the output: The class with the best score
+        all_predicted.extend([int(v) for v in predicted])
+        all_labels.extend([int(v) for v in labels])
+    return all_predicted,all_labels
